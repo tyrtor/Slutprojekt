64a1b7828a419cec319c2a840c7a214e2533fc80
tyrtor <tyrtor2@gmail.com>
2020-05-19 14:32:09 +0200

    förtsatt att koda på spelet

--- pong.js
+++ pong.js
@@ -13,8 +13,8 @@ class Boll {
     constructor() {
         this.x = eCanvas.width / 2,
             this.y = eCanvas.height / 2,
-            this.dy = -5,
-            this.dx = 0,
+            this.dy = 0,
+            this.dx = 3,
             this.radie = 12
 
     }
@@ -91,12 +91,22 @@ class Racketen {
 /* skapa en variabel för classen */
 var racketen = new Racketen();
 
+class Poäng{
+    constructor(){
+        this.h = 0,
+        this.v = 0
+    }
+}
+
+var poäng = new Poäng();
+
 /* Spelloopen */
 function gameLoop() {
     bakgrund.rita();
     racketen.rita();
     boll.rita();
     animate();
+    kollision();
 
     rörselseBoll();
 
@@ -114,6 +124,18 @@ function rörselseBoll() {
 }
 
 /* röra på racketen */
+/* högra racketen */
+window.addEventListener("keyup", function(e) {
+    switch (e.key) {
+        case "ArrowUp":
+            racketen.hupp = false;
+            break;
+        case "ArrowDown":
+            racketen.hned = false;
+            break;
+    }
+})
+
 window.addEventListener("keydown", function(e) {
     switch (e.key) {
         case "ArrowUp":
@@ -126,18 +148,14 @@ window.addEventListener("keydown", function(e) {
             break;
     }
 })
-window.addEventListener("keyup", function(e) {
+
+/* vänstra racketen */
+window.addEventListener("keyup", function (e) {
     switch (e.key) {
-        case "ArrowUp":
-            racketen.hupp = false;
-            break;
-        case "ArrowDown":
-            racketen.hned = false;
-            break;
-        case "KeyW":
+        case 87:
             racketen.vupp = false;
             break;
-        case "KeyS":
+        case 83:
             racketen.vned = false;
             break;
     }
@@ -145,28 +163,57 @@ window.addEventListener("keyup", function(e) {
 
 window.addEventListener("keydown", function(e) {
     switch (e.key) {
-        case "87":
+        case 87:
             racketen.vupp = true;
             console.log(e.key);
             break;
-        case "83":
+        case 83:
             racketen.vned = true;
             console.log(e.key);
             break;
     }
 })
 
+
 function animate() {
+
     if (boll.y <= 15) {
         boll.dy = -boll.dy
     }
-    if (boll.y > 600) {
-        boll.dy = +boll.dy
+    if (boll.y >= 590) {
+        boll.dy = -boll.dy
     }
     if (racketen.hupp == true) {
         racketen.hy --;
     }
     if (racketen.hned == true) {
         racketen.hy ++;
+    }
+    if (racketen.vupp == true) {
+        racketen.vy --;
+    }
+    if (racketen.vned == true) {
+        racketen.vy ++;
+    }
+}
+
+
+
+function kollision() {
+    if ((racketen.hy <= boll.y) && (boll.y <= racketen.hy)) {
+        if (boll.x  + boll.radie >= racketen.hx) {
+            boll.dx = -boll.dx;
+            poäng.h ++;
+            
+        }
+    }
+
+    if ((racketen.vy <= boll.y) && (boll.y <= racketen.vy)) {
+        if (boll.x - boll.radie <= racketen.vx) {
+            boll.dx = -boll.dx;
+            poäng.v ++;
+            console.log(poäng.v);
+            
+        }
     }
 }
